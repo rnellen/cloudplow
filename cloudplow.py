e97a370a517713f6276022f4234fedd4ad675c6e
Roberto Nellen <34263242+rnellen@users.noreply.github.com>
2021-07-08 12:34:00 +0200

    add post script

--- cloudplow.py
+++ cloudplow.py
@@ -5,6 +5,7 @@ import sys
 import time
 from logging.handlers import RotatingFileHandler
 from multiprocessing import Process
+import subprocess
 
 import requests
 import schedule
@@ -471,6 +472,7 @@ def do_upload(remote=None):
             log.exception("Exception occurred while uploading: ")
 
     log.info("Finished upload")
+    do_postscript(uploader_config['post_script'])
 
 
 @decorators.timed
@@ -722,6 +724,15 @@ def do_plex_monitor():
     log.info("Finished monitoring Plex stream(s)!")
     plex_monitor_thread = None
 
+ def do_postscript(script):
+    if os.path.isfile(script)==False:
+        log.error("Script file does not exist")
+    else:
+        log.info("Script File: %s is running",script)
+        try:
+            subprocess.call(script)
+        except:
+            log.error("Please Make sure your script has a shell, and is properly formated" )
 
 ############################################################
 # SCHEDULED FUNCS
